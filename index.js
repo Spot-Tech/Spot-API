38249f82d1c1706d362fa12e190805432cb817a5
Steve Tan <i@stevetan.me>
2019-09-16 22:37:58 +0800

    Add server skeleton

---
+++ index.js
@@ -0,0 +1,5 @@
+// Run all scripts with ESM syntax support.
+
+require = require("esm")(module/*, options*/);
+
+module.exports = require("./server.js"); // Start server
---
+++ server.js
@@ -0,0 +1,19 @@
+import serverConfig from "./config/server.json";
+
+import Hapi from "@hapi/hapi";
+
+const init = async () => {
+
+    const server = Hapi.server(serverConfig);
+
+    await server.start();
+
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
